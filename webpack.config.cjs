d0e4e1df238547d7eb4d3661d0134a48ed414b56
maangie <maangie906@gmail.com>
2024-04-24 11:54:40 +0900

    本番モードへの変更とバンドル最適化の実施
    - 開発モードから本番モードへ変更。これにより、バンドル時の最適化が自動的に行われる。
    - ファイル名のパターンを変更し、各ビルドで一意のバンドルが生成されるように設定。これはキャッシュ管理を助ける。
    - チャンクファイル名の設定を追加し、非同期ロードされるコードブロックが識別しやすくなる。
    - outputにcleanオプションを追加してビルドディレクトリをクリーンアップ。古いファイルが自動的に削除され、出力がクリーンに保たれる。
    - optimization設定を追加してツリーシェイキングを有効にし、コード分割を詳細に設定。これにより初回読み込みサイズを最適化し、パフォーマンスを向上。
    - cacheGroupsを設定し、node_modulesからのベンダーライブラリとその他のモジュールで異なるキャッシング戦略を適用。これにより、より効果的なキャッシュ利用が可能に。
    - performanceの設定を通じて、エントリポイントとアセットサイズの上限を設定し、大きなアセットに対する警告を管理。

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.ts',
   module: {
     rules: [
@@ -17,8 +17,42 @@ module.exports = {
     extensions: ['.tsx', '.ts', '.js']
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].chunk.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  optimization: {
+    usedExports: true,
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      maxSize: 244000,
+      minChunks: 1,
+      maxAsyncRequests: 6,
+      maxInitialRequests: 4,
+      automaticNameDelimiter: '~',
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+          minSize: 20000,
+          maxSize: 50000
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+          minSize: 10000,
+          maxSize: 30000
+        },
+      },
+    }
+  },
+  performance: {
+    maxEntrypointSize: 609000,
+    maxAssetSize: 609000
   },
   devServer: {
     static: {
